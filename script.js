21ff22185fefaa30c7c59acb14666036ac1c94c1
stiyqr <stiyqr@gmail.com>
2026-02-19 03:32:48 +0800

    add contact section

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function copyEmail() {
+    const email = "lukitaveronica@gmail.com";
+    const tooltip = document.querySelector(".tooltip");
+    
+    navigator.clipboard.writeText(email).then(() => {
+        const tooltiptext = document.querySelector(".tooltiptext");
+        tooltiptext.textContent = "Copied!";
+
+        tooltip.addEventListener("mouseleave", function reset() {
+            tooltiptext.textContent = "Copy email";
+            tooltip.removeEventListener("mouseleave", reset);
+        });
+
+    }).catch((err) => {
+        console.error("Failed to copy email: ", err);
+    });
+
+
+    // setTimeout(() => {
+    //     tooltiptext.textContent = "Copy email";
+    // }, 2000);
+}
